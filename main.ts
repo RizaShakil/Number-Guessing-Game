0e32291eb5a044c4ea168e76d3d6743255e0aab8
RizaShakil <rizashakeel43@gmail.com>
2024-03-30 10:24:16 -0700

    Number_Guessing_Game

---
+++ main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+console.log("\t\n\t Welcome To Riza Shakeel Number_Guessing_Game\t \n\t");
+
+const randomnumber =Math.floor(Math.random()* 20 + 1);
+
+const answer = await inquirer.prompt(
+    [
+        {
+            name: "useguessingnumber",
+            type: "number",
+            message: "Enter your guess number(Number LImit 1 to 20)",
+        }
+    ]
+);
+
+if (answer.useguessingnumber === randomnumber){
+    console.log(chalk.magenta("\n\tCongratulation! you guess a correct number\t\n"))
+}
+else{
+    console.log(chalk.blueBright("\n\tSorry you guess a wrong number\t\n"))
+};
